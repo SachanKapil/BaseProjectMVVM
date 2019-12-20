a069584d9698c9e50f47aa6dead7b33aa38513ba
Kapil <kapilsachan6@gmail.com>
2019-12-20 14:38:38 +0530

    Base project setup

--- app/src/main/java/com/baseprojectmvvm/data/model/onboarding/User.java
+++ app/src/main/java/com/baseprojectmvvm/data/model/onboarding/User.java
@@ -4,108 +4,37 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class User {
-
-
-    @SerializedName("middle_name")
-    @Expose
-    private String middleName;
-    @SerializedName("last_name")
-    @Expose
-    private String lastName;
-    @SerializedName("gender")
-    @Expose
-    private String gender;
-    @SerializedName("dob")
-    @Expose
-    private String dob;
-    @SerializedName("age")
-    @Expose
-    private Integer age;
     @SerializedName("phone")
     @Expose
     private String phone;
-    @SerializedName("address")
-    @Expose
-    private String address;
-    @SerializedName("user_lat")
-    @Expose
-    private String userLat;
-    @SerializedName("user_long")
-    @Expose
-    private String userLong;
-    @SerializedName("country_id")
-    @Expose
-    private String countryId;
-    @SerializedName("state_id")
-    @Expose
-    private String stateId;
-    @SerializedName("city_id")
-    @Expose
-    private String cityId;
     @SerializedName("email")
     @Expose
     private String email;
     @SerializedName("first_name")
     @Expose
     private String firstName;
-    @SerializedName("registered_date")
-    @Expose
-    private String registeredDate;
     @SerializedName("token")
     @Expose
     private String accessToken;
     @SerializedName("image")
     @Expose
     private String image;
-    @SerializedName("image_thumb")
-    @Expose
-    private String imageThumb;
     @SerializedName("user_id")
     @Expose
     private Integer userId;
-    @SerializedName("status")
+    @SerializedName("refresh_token")
     @Expose
-    private String status;
+    private String refreshToken;
 
-    public String getMiddleName() {
-        return middleName;
-    }
+    private String device_id;
 
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
+    private String platform;
 
-    public String getLastName() {
-        return lastName;
-    }
+    private String password;
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    private String device_token;
 
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+    private String confirmPassword;
 
     public String getPhone() {
         return phone;
@@ -113,54 +42,6 @@ public class User {
 
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getUserLat() {
-        return userLat;
-    }
-
-    public void setUserLat(String userLat) {
-        this.userLat = userLat;
-    }
-
-    public String getUserLong() {
-        return userLong;
-    }
-
-    public void setUserLong(String userLong) {
-        this.userLong = userLong;
-    }
-
-    public String getCountryId() {
-        return countryId;
-    }
-
-    public void setCountryId(String countryId) {
-        this.countryId = countryId;
-    }
-
-    public String getStateId() {
-        return stateId;
-    }
-
-    public void setStateId(String stateId) {
-        this.stateId = stateId;
-    }
-
-    public String getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(String cityId) {
-        this.cityId = cityId;
     }
 
     public String getEmail() {
@@ -179,14 +60,6 @@ public class User {
         this.firstName = firstName;
     }
 
-    public String getRegisteredDate() {
-        return registeredDate;
-    }
-
-    public void setRegisteredDate(String registeredDate) {
-        this.registeredDate = registeredDate;
-    }
-
     public String getAccessToken() {
         return accessToken;
     }
@@ -203,14 +76,6 @@ public class User {
         this.image = image;
     }
 
-    public String getImageThumb() {
-        return imageThumb;
-    }
-
-    public void setImageThumb(String imageThumb) {
-        this.imageThumb = imageThumb;
-    }
-
     public Integer getUserId() {
         return userId;
     }
@@ -219,28 +84,12 @@ public class User {
         this.userId = userId;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public String getRefreshToken() {
         return refreshToken;
     }
 
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
     }
 
     public String getDevice_id() {
@@ -282,23 +131,5 @@ public class User {
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
     }
-
-    @SerializedName("refresh_token")
-    @Expose
-    private String refreshToken;
-
-    @SerializedName("oldpassword")
-    @Expose
-    private String oldPassword;
-
-    private String device_id;
-
-    private String platform;
-
-    private String password;
-
-    private String device_token;
-
-    private String confirmPassword;
 
 }
